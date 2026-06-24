f42db92afd77bfe24e806431f2e4d5c00430cef8
hamza <xxxdarkxlordxxx3@gmail.com>
2026-06-24 20:23:50 +0300

    Add files via upload

---
+++ super_methods.js
@@ -0,0 +1,23 @@
+Array.prototype.numExport = function () {
+    return this.filter((e, i, a) => typeof e === "number");
+}
+Array.prototype.removeRepeats = function () {
+    return [...new Set(this)];
+}
+Array.prototype.isEmpty = function () {
+    if (this.length == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+Array.prototype.clearArray = function () {
+    return this.length = 0;
+}
+Array.prototype.randomIndexNum = function () {
+    if (this.length === 0)
+        return undefined;
+    else
+        return this[Math.floor(Math.random() * this.length)];
+}
